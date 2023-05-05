8129cba82980f71e57d160c6faea77cc9aaf29c0
n-b.appaiah <n-b.appaiah@sgssupport.abudhabi.ae>
2023-05-05 08:13:16 +0530

    replaced var with let

--- Lab-1MVVM/MovieListView/Model/MovieList.swift
+++ Lab-1MVVM/MovieListView/Model/MovieList.swift
@@ -8,31 +8,31 @@
 import Foundation
 
 struct MovieList: Codable {
-    var created_by: String
-    var description: String
-    var favorite_count: Int
-    var id: String
-    var items : [ItemList]
-    var item_count: Int
-    var iso_639_1: String
-    var name: String
-    var poster_path: String
+    let created_by: String
+    let description: String
+    let favorite_count: Int
+    let id: String
+    let items : [ItemList]
+    let item_count: Int
+    let iso_639_1: String
+    let name: String
+    let poster_path: String
 }
 
 struct ItemList : Codable {
-    var adult: Bool
-    var backdrop_path: String
-    var genre_ids: [Int]
-    var id: Int
-    var media_type: String
-    var original_language: String
-    var original_title: String
-    var overview: String
-    var popularity: Float
-    var poster_path: String
-    var release_date: String
-    var title: String
-    var video: Bool
-    var vote_average: Float
-    var vote_count: Int
+    let adult: Bool
+    let backdrop_path: String
+    let genre_ids: [Int]
+    let id: Int
+    let media_type: String
+    let original_language: String
+    let original_title: String
+    let overview: String
+    let popularity: Float
+    let poster_path: String
+    let release_date: String
+    let title: String
+    let video: Bool
+    let vote_average: Float
+    let vote_count: Int
 }
